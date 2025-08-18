aedabcd80f499ea352401f71243c0260c9ee8c59
JhonardVictorillo <victorillojhonard@gmail.com>
2025-08-18 16:59:37 +0800

    praktis deploy

---
+++ script.js
@@ -0,0 +1,8 @@
+
+    const hamburger = document.querySelector('.hamburger');
+    const sidebar = document.querySelector('.sidebar');
+
+    hamburger.addEventListener('click', () => {
+        sidebar.classList.toggle('active');
+        hamburger.classList.toggle('active');
+    });
