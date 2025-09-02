95ddd3ab4d01c086e24d356263c50376e52698d7
JhonardVictorillo <victorillojhonard@gmail.com>
2025-09-02 16:29:25 +0800

    update db port

--- config.php
+++ config.php
@@ -4,10 +4,10 @@ $host = getenv('DB_HOST');
 $dbname = getenv('DB_NAME');
 $username = getenv('DB_USER');
 $password = getenv('DB_PASS');      // Change as needed
-
+$port = getenv('DB_PORT') ?: 3306; // Default to 3306 if not set
 // Create database connection
 try {
-    $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $conn = new PDO("mysql:host=$host;dbname=$dbname;port=$port", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(PDOException $e) {
     echo json_encode(['error' => 'Connection failed: ' . $e->getMessage()]);
